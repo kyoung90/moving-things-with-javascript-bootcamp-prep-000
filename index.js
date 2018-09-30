fecad2ab70e0c1995c9ae84be96e547bf28a3f05
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-09-30 23:43:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,3 +22,9 @@ document.addEventListener('keydown', function(e) {
     moveDodgerLeft()
   }
 })
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 38) {
+    moveDodgerLeft()
+  }
+})
